39ed85759cacaa4560ce544ada1275344506e044
Nirvankoo <127167254+Nirvankoo@users.noreply.github.com>
2023-08-29 16:51:13 -0400

    new changes

--- button.cpp
+++ button.cpp
@@ -6,41 +6,42 @@
 #include "button.h"
 #include "game.h"
 using namespace std;
+// comment
 
 const int NUMBER_OF_QUESTIONS = 30;
 const int NUMBER_OF_ANSWERS = 4;
 int current_question = 0;
-bool right_answer[NUMBER_OF_QUESTIONS][NUMBER_OF_ANSWERS] = {{true, false, false, false}, // 1st question
-                                                             {false, true, false, false}, //2nd question
-                                                             {false, false, true, false}, //3rd question
-                                                             {false, true, false, false}, //4th question
-                                                             {false, true, false, false}, //5th question
-                                                             {false, false, true, false}, //6th question
-                                                             {false, true, false, false}, //7th question
-                                                             {false, false, true, false}, //8th question  
-                                                             {true, false, false, false}, //9th question
-                                                             {true, false, false, false}, //10  question
-                                                             {true, false, false, false}, //11th question
-                                                             {false, false, false, true}, //12th question
-                                                             {false, false, true, false}, //13th question
-                                                             {true, false, false, false}, //14th question
-                                                             {false, false, true, false}, //15th question
-                                                             {false, false, true, false}, //16th question
-                                                             {true, false, false, false}, //17th question
-                                                             {false, false, false, true}, //18th question   
-                                                             {true, false, false, false}, //19th question
-                                                             {false, true, false , false}, //20th question
-                                                             {false, false, true, false}, //21th question
-                                                             {false, false, true, false}, //22th question
-                                                             {false, false, false, true}, //23th question
-                                                             {true, false, false, false}, //24th question
-                                                             {true, false, false, false}, //25th question
-                                                             {false, false, true, false}, //26th question
-                                                             {false, false, true, false}, //27th question
-                                                             {false, true, false, false}, //28th question
-                                                             {true, false, false, false}, //29th question
-                                                             {false, false, false, true}};  //30th question
-int right_answer_flag = 1;
+bool right_answer[NUMBER_OF_QUESTIONS][NUMBER_OF_ANSWERS] = {{true, false, false, false},  // 1st question
+                                                             {false, true, false, false},  // 2nd question
+                                                             {false, false, true, false},  // 3rd question
+                                                             {false, true, false, false},  // 4th question
+                                                             {false, true, false, false},  // 5th question
+                                                             {false, false, true, false},  // 6th question
+                                                             {false, true, false, false},  // 7th question
+                                                             {false, false, true, false},  // 8th question
+                                                             {true, false, false, false},  // 9th question
+                                                             {true, false, false, false},  // 10  question
+                                                             {true, false, false, false},  // 11th question
+                                                             {false, false, false, true},  // 12th question
+                                                             {false, false, true, false},  // 13th question
+                                                             {true, false, false, false},  // 14th question
+                                                             {false, false, true, false},  // 15th question
+                                                             {false, false, true, false},  // 16th question
+                                                             {true, false, false, false},  // 17th question
+                                                             {false, false, false, true},  // 18th question
+                                                             {true, false, false, false},  // 19th question
+                                                             {false, true, false, false},  // 20th question
+                                                             {false, false, true, false},  // 21th question
+                                                             {false, false, true, false},  // 22th question
+                                                             {false, false, false, true},  // 23th question
+                                                             {true, false, false, false},  // 24th question
+                                                             {true, false, false, false},  // 25th question
+                                                             {false, false, true, false},  // 26th question
+                                                             {false, false, true, false},  // 27th question
+                                                             {false, true, false, false},  // 28th question
+                                                             {true, false, false, false},  // 29th question
+                                                             {false, false, false, true}}; // 30th question
+//int right_answer_flag = 1;
 
 SDL_Renderer *renderer = NULL;
 
@@ -65,7 +66,7 @@ Button_texture::~Button_texture()
     button_position.y = 0;
 }
 
-bool Button_texture::load_from_file(string path, bool is_question)
+bool Button_texture::load_from_file(string path, bool is_question, bool is_hint)
 {
     SDL_Surface *loaded_surface = IMG_Load(path.c_str());
     if (loaded_surface == NULL)
@@ -84,16 +85,20 @@ bool Button_texture::load_from_file(string path, bool is_question)
         }
         else
         {
-            if (!is_question)
+        if(!is_question && !is_hint)
             {
                 width = BUTTON_WIDTH;
                 height = BUTTON_HEIGHT;
             }
-            else
+            else if(is_hint)
             {
-                width = loaded_surface->w;
-                height = loaded_surface->h;
-            }
+                width = BUTTON_HINTS_WIDTH;
+                height = BUTTON_HINTS_HEIGHT;
+                }
+                else{
+                    width = loaded_surface->w;
+                    height = loaded_surface->h;
+                    }
         }
         SDL_FreeSurface(loaded_surface);
     }
@@ -129,11 +134,12 @@ void Button_texture::set_button_position(int x, int y)
 
 void Button_texture::handle_button_event(SDL_Event *e)
 {
-    if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP)
+    if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN)
     {
         int x, y;
         SDL_GetMouseState(&x, &y);
         bool inside = true;
+
         if (x < button_position.x)
         {
             inside = false;
@@ -164,17 +170,18 @@ void Button_texture::handle_button_event(SDL_Event *e)
             case SDL_MOUSEBUTTONDOWN:
                 current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
                 break;
-            case SDL_MOUSEBUTTONUP:
-                current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
-                break;
+            
             }
         }
     }
 }
 
+// bottons objsects!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 Button_texture button_texture[TOTAL_BUTTONS];
 Button_texture question_texture;
 SDL_Rect button_sprite_clips[BUTTON_SPRITE_TOTAL];
+SDL_Rect button_hint_clips[BUTTON_SPRITE_TOTAL];
 
 bool init_button_texture()
 {
@@ -234,34 +241,64 @@ bool load_button_media()
     bool success = true;
     for (int i = 0; i < TOTAL_BUTTONS; i++)
     {
-
-        string path = "image/q" + to_string(current_question) + "/button" + to_string(i) + ".jpg";
-        cout << path << ": is loaded!" << endl;
-
-        if (!button_texture[i].load_from_file(path, false))
+        if (i < 4)
         {
-            cout << "Failed to load button texture image!" << endl;
+            string path = "image/q" + to_string(current_question) + "/button" + to_string(i) + ".jpg";
+            cout << path << ": is loaded!" << endl;
+            if (!button_texture[i].load_from_file(path, false, false))
+            {
+                cout << "Failed to load button texture image!" << endl;
 
-            success = false;
+                success = false;
+            }
+        }
+        else
+        {
+            string path = "image/hint" + to_string(i - 4) + ".jpg";
+            if (!button_texture[i].load_from_file(path, false, true))
+            {
+                cout << "Failed to load button texture image HERE!" << endl;
+
+                success = false;
+                success = false;
+                success = false;
+            }
+            cout << path << ": is loaded!" << endl;
         }
     }
-    question_texture.load_from_file("image/q" + to_string(current_question) + "/question.jpg", true);
-        for (int i = 0; i < 4; i++)
-        {
 
-            button_sprite_clips[i].x = 0;
-            button_sprite_clips[i].y = i * BUTTON_HEIGHT;
-            button_sprite_clips[i].w = BUTTON_WIDTH;
-            button_sprite_clips[i].h = BUTTON_HEIGHT;
-        }
+    question_texture.load_from_file("image/q" + to_string(current_question) + "/question.jpg", true, false);
+    for (int i = 0; i < 4; i++)
+{
+    button_sprite_clips[i].x = 0;
+    button_sprite_clips[i].y = i * BUTTON_HEIGHT;
+    button_sprite_clips[i].w = BUTTON_WIDTH;
+    button_sprite_clips[i].h = BUTTON_HEIGHT;
+}
+for(int i = 0; i < 3; i++){
+   
+    
+    button_hint_clips[i].x = 0;
+    button_hint_clips[i].y = i * BUTTON_HINTS_HEIGHT;
+    button_hint_clips[i].w = BUTTON_HINTS_WIDTH;
+    button_hint_clips[i].h = BUTTON_HINTS_HEIGHT;
+    
+}
+
+
+
 
 
     button_texture[0].set_button_position(285, 465);
     button_texture[1].set_button_position(285, 545);
     button_texture[2].set_button_position(285, 615);
     button_texture[3].set_button_position(285, 690);
-    question_texture.set_button_position(300, 300);
 
+    button_texture[4].set_button_position(150, 50);
+    button_texture[5].set_button_position(400, 50);
+    button_texture[6].set_button_position(650, 50);
+
+    question_texture.set_button_position(300, 300);
     return success;
 }
 
@@ -305,10 +342,14 @@ void create_buttons()
             SDL_Quit();
             return;
         }
+        
+            
 
-        right_answer_flag = 1; // reset right_answer_flag
+
+        //right_answer_flag = 1; // reset right_answer_flag
 
         bool quit = false;
+
         SDL_Event e;
 
         while (!quit)
@@ -320,7 +361,7 @@ void create_buttons()
                     quit = true;
                 }
 
-                for (int i = 0; i < TOTAL_BUTTONS; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     button_texture[i].handle_button_event(&e);
 
@@ -329,22 +370,42 @@ void create_buttons()
                         if (right_answer[current_question][i])
                         {
                             cout << "Correct!" << endl;
-                           
-                            right_answer_flag = 1;
+
+                            //right_answer_flag = 1;
                             quit = true;
                             current_question++;
                             button_texture[i].current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
                         }
                         else
                         {
-                            right_answer_flag = 2;
+                            //right_answer_flag = 2;
                             current_question++;
                             button_texture[i].current_sprite = BUTTON_SPRITE_MOUSE_UP;
-                          
+
                             cout << "Wrong!" << endl;
                             quit = true;
                         }
-                        
+                    }
+                }
+
+                //hint
+                for (int i = 4; i < 7; i++)
+                {
+                    button_texture[i].handle_button_event(&e);
+
+                    if (e.type == SDL_MOUSEBUTTONDOWN && button_texture[i].current_sprite == BUTTON_SPRITE_MOUSE_DOWN)
+                    {
+                        if(i == 4){
+                            button_texture[i].current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
+                        }
+                        else if(i == 5){
+                            button_texture[i].current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
+                            
+                        }
+                        else if(i == 6){
+                            button_texture[i].current_sprite = BUTTON_SPRITE_MOUSE_DOWN;
+                            
+                        }   
                     }
                 }
             }
@@ -352,26 +413,27 @@ void create_buttons()
             SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, existing_texture, NULL, NULL);
 
-            for (int i = 0; i < TOTAL_BUTTONS; i++)
-            { 
-              
-        
+            for (int i = 0; i < 4; i++)
+            {
+
                 button_texture[i].button_render(button_texture[i].button_position.x, button_texture[i].button_position.y, &button_sprite_clips[button_texture[i].current_sprite]);
-                question_texture.button_render(question_texture.button_position.x, question_texture.button_position.y, NULL);
-                
             }
+            for(int i = 4; i < TOTAL_BUTTONS; i++){
+               button_texture[i].button_render(button_texture[i].button_position.x, button_texture[i].button_position.y, &button_hint_clips[button_texture[i].current_sprite]);
+            }
+            question_texture.button_render(question_texture.button_position.x, question_texture.button_position.y, NULL);
 
             SDL_RenderPresent(renderer);
-            
-           
         }
-       
-        cout << "current_question: " << current_question << endl;
+
+        cout << "Сurrent question is: " << current_question << endl;
         SDL_Delay(2000); // Pause for 2 seconds
-        
     }
+    
+
     SDL_DestroyTexture(existing_texture);
     close_button_texture();
+
     SDL_DestroyRenderer(renderer);
 
     SDL_DestroyWindow(main_window);
--- button.h
+++ button.h
@@ -7,11 +7,16 @@
 
 const int BUTTON_WIDTH = 300;
 const int BUTTON_HEIGHT = 30;
-const int TOTAL_BUTTONS = 4;
 
+const int BUTTON_HINTS_WIDTH = 170;
+const int BUTTON_HINTS_HEIGHT = 75;
+
+const int TOTAL_BUTTONS = 7;
+const int TOTAL_HINTS = 3;
 
 enum Button_current_sprite{
     BUTTON_SPRITE_MOUSE_OUT = 0,
+    
     BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
     BUTTON_SPRITE_MOUSE_DOWN = 2,
     BUTTON_SPRITE_MOUSE_UP = 3,
@@ -23,12 +28,13 @@ class Button_texture{
     public:
     Button_texture();
     ~Button_texture();
-    bool load_from_file(std::string path, bool is_question);
+    bool load_from_file(std::string path, bool is_question, bool is_hint);
     void button_render(int x, int y, SDL_Rect* clip = NULL);
     int get_width();
     int get_height();
     void set_button_position(int x, int y);
     void handle_button_event(SDL_Event* e);
+
     SDL_Point button_position;
     Button_current_sprite current_sprite;
     private:
